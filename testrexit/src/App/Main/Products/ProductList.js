f060c6f9110c51d820270cdd0ebbc0dee7108065
Denys Nefodov <nefodovden@gmail.com>
2021-08-04 21:26:32 +0300

    add product from products

--- testrexit/src/App/Main/Products/ProductList.js
+++ testrexit/src/App/Main/Products/ProductList.js
@@ -1,39 +1,32 @@
 import React from 'react'
 import ProductListItem from './ProductListItem'
+import products from './products'
 
 const ProductList = () => {
 	return (
 		<>
             <h1 className="page-title">ProductList</h1>
             <div className="product-list">
-                <ProductListItem
-                    name="iPhone X"
-                    description="This is iPhone X"
-                    type="phone"
-                    capacity="64 GB"
-                    price="1000"
-                />
-                <ProductListItem
-                    name="iPhone XS"
-                    description="This is iPhone XS"
-                    type="phone"
-                    capacity="256 GB"
-                    price="1230"
-                />
-                <ProductListItem
-                    name="iPhone XR"
-                    description="This is iPhone XR"
-                    type="phone"
-                    capacity="128 GB"
-                    price="1300"
-                />
-                <ProductListItem
-                    name="iPhone 11"
-                    description="This is iPhone 11"
-                    type="phone"
-                    capacity="512 GB"
-                    price="1400"
-                />
+                {
+                    products.map(({
+                        id,
+                        name,
+                        description,
+                        type,
+                        capacity,
+                        price
+                    }
+                    ) => (
+                            <ProductListItem
+                            key={id}
+                            name={name}
+                            description={description}
+                            type={type}
+                            capacity={capacity}
+                            price={price}
+                        />
+                    ))
+                }
             </div>
         </>
 	)
---
+++ testrexit/src/App/Main/Products/products.js
@@ -0,0 +1,36 @@
+const products = [
+    {
+        id:1,
+        name:"iPhone X",
+        description:"This is iPhone X",
+        type:"phone",
+        capacity:"64 GB",
+        price:"1000",
+    },
+    {
+        id:2,
+        name:"iPhone XS",
+        description:"This is iPhone XS",
+        type:"phone",
+        capacity:"256 GB",
+        price:"1230",
+    },
+    {
+        id:3,
+        name:"iPhone XR",
+        description:"This is iPhone XR",
+        type:"phone",
+        capacity:"512 GB",
+        price:"1300",
+    },
+    {
+        id:4,
+        name:"iPhone 11",
+        description:"This is iPhone 11",
+        type:"phone",
+        capacity:"128 GB",
+        price:"1111",
+    },
+]
+
+export default products
