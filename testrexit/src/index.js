3b82cf568e4b8f3652e5bfd953d05148c75cc61f
Denys Nefodov <nefodovden@gmail.com>
2021-08-04 14:13:41 +0300

    add first react components

--- testrexit/src/index.js
+++ testrexit/src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const h1 = <h1 id="test1">Hello</h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Text = () => {
+    return (
+        <React.Fragment>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, esse!</p>
+        </React.Fragment>
+    )
+}
+const List = () => {
+    return (
+        <ul>
+            <li>Lorem, ipsum dolor.</li>
+            <li>Mollitia, quis vitae.</li>
+            <li>Laborum, exercitationem vel.</li>
+            <li>Et, doloribus. Inventore?</li>
+            <li>Doloremque, molestiae saepe.</li>
+        </ul>
+    )
+}
+
+const Hi = () => {
+    return (
+        <React.Fragment>
+            <h1>Hiiiiii</h1>
+        </React.Fragment>
+    )
+}
+
+const App = () => {
+    return (
+        <>
+            <Text/>
+            <List/>
+            <Hi/>
+        </>
+    )
+}
+
+
+
+
+ReactDOM.render(<App/>, document.getElementById("root"))
