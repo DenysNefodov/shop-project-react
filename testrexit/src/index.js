991b9d1ab6c2cb74cdc2eb45b5259e49e6d485ee
Denys Nefodov <nefodovden@gmail.com>
2021-08-04 15:55:06 +0300

    added basic application structure

--- testrexit/src/index.js
+++ testrexit/src/index.js
@@ -1,41 +1,58 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// const h1 = <h1 id="test1">Hello</h1>
-
-const Text = () => {
-    return (
-        <React.Fragment>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, esse!</p>
-        </React.Fragment>
-    )
-}
-const List = () => {
-    return (
-        <ul>
-            <li>Lorem, ipsum dolor.</li>
-            <li>Mollitia, quis vitae.</li>
-            <li>Laborum, exercitationem vel.</li>
-            <li>Et, doloribus. Inventore?</li>
-            <li>Doloremque, molestiae saepe.</li>
-        </ul>
-    )
-}
-
-const Hi = () => {
-    return (
-        <React.Fragment>
-            <h1>Hiiiiii</h1>
-        </React.Fragment>
-    )
-}
+import "./reset.css"
+import "./base.css"
+import "./header.css"
+import "./menu.css"
+import "./cart.css"
 
 const App = () => {
     return (
         <>
-            <Text/>
-            <List/>
-            <Hi/>
+            <header class="header">
+				<div class="container">
+					<div class="header-row">
+					<div class="logo text-center">
+						<img src="img/logo.png" alt=""/>
+					</div>
+
+					<div class="menu">
+						<ul>
+						<li><a href="">Home</a></li>
+						<li><a href="">Products</a></li>
+						<li><a href="">Payment</a></li>
+						<li><a href="">Shipping</a></li>
+						</ul>
+					</div>
+
+					<div class="cart text-center">
+						<div class="products-count">0</div>
+						<div class="products-price">$500</div>
+					</div>
+
+					</div>
+				</div>
+            </header>
+            <main class="main">
+				<div class="container">
+					<div class="row">
+					<div class="aside">
+						Filter
+					</div>
+					<div class="main-content">
+						Product List
+					</div>
+					</div>
+				</div>
+            </main>
+            <footer class="footer">
+				<div class="container">
+					<div class="row">
+					Footer
+					</div>
+				</div>
+            </footer>
         </>
     )
 }
