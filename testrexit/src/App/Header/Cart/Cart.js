603732cff6f8cda0f0a22dd1ed98c00abd014c89
Denys Nefodov <nefodovden@gmail.com>
2021-08-05 19:40:56 +0300

    add method reduce

--- testrexit/src/App/Header/Cart/Cart.js
+++ testrexit/src/App/Header/Cart/Cart.js
@@ -3,10 +3,14 @@ import React from 'react'
 import "./cart.css"
 import products from '../../Main/Products/products'
 
+
+const productsObject = products.reduce((obj, product) => ({
+    ...obj,
+    [product.id]:product
+}), {})
+
 const Cart = ({
     productsInCart
-
-
 }) => {
     return (
         <div className="cart text-center">
