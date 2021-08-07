60a9762b4b70533fe738c381acb5d18da6502571
Denys Nefodov <nefodovden@gmail.com>
2021-08-07 17:11:16 +0300

    use hooks on change color

--- testrexit/src/Components/Testimonials/Testimonials.js
+++ testrexit/src/Components/Testimonials/Testimonials.js
@@ -3,11 +3,19 @@ import React, { useState } from 'react'
 const Testimonials = () => {
 
     const [count, setCount] = useState(1)
+    const [color, setColor] = useState("green")
 
 
     return (
         <>
             <h1 className="page-title">Testimonials</h1>
+
+            <div>
+                Color:{color}
+                <div>
+                    <button onClick={() => setColor("red")}>Change color</button>
+                </div>
+            </div>
 
             <button onClick={() => setCount(count - 1)}>-</button>
             <input type="text" value={count}/>
